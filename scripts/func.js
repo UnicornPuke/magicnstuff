5ee87cbc89436acf89270c1c002ee3aa7807a898
UnicornPuke <41150712+UnicornPuke@users.noreply.github.com>
2023-09-10 17:22:56 +0000

    basic ui

---
+++ scripts/func.js
@@ -0,0 +1,5 @@
+currentURL = window.location.hostname
+
+function redir(a) {
+    window.location.href = currentURL + "/" + a;
+}
---
+++ scripts/redirectwiki.js
@@ -0,0 +1,4 @@
+const currentUrl = window.location.hostname;
+
+let url = "https://" + currentUrl + "/wiki/home-page";
+window.location.href = url;
