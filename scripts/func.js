6cc3c7d8c44c29d1884f011e44d74aea0cc914d0
UnicornPuke <41150712+UnicornPuke@users.noreply.github.com>
2023-09-20 00:37:38 +0000

    things

--- scripts/func.js
+++ scripts/func.js
@@ -1,5 +1,21 @@
-currentURL = window.location.hostname
-
-function redir(a) {
-    window.location.href = currentURL + "/" + a;
+function thing(a) {
+    bottom.style.visibility = 'visible';
+    if (a == 's') {
+        bottom.innerHTML = 'Reconstruction&nbsp;&nbsp;&nbsp;&nbsp;Reanimation&nbsp;&nbsp;&nbsp;&nbsp;Destruction&nbsp;&nbsp;&nbsp;&nbsp;Creation&nbsp;&nbsp;&nbsp;&nbsp;Manipulation&nbsp;&nbsp;&nbsp;&nbsp;Naturization&nbsp;&nbsp;&nbsp;&nbsp;Naturization&nbsp;&nbsp;&nbsp;&nbsp;Innovation&nbsp;&nbsp;&nbsp;&nbsp;Transportation'
+    }
+    else if (a == 'r') {
+        bottom.innerHTML = '<a href="https://redesigned-parakeet-46rgw7jv737gr7-8000.app.github.dev/wiki/races/human">Human</a>&nbsp;&nbsp;&nbsp;&nbsp;Elemental&nbsp;&nbsp;&nbsp;&nbsp;Aplysia&nbsp;&nbsp;&nbsp;&nbsp;Elf&nbsp;&nbsp;&nbsp;&nbsp;Half Elf&nbsp;&nbsp;&nbsp;&nbsp;Gremlin&nbsp;&nbsp;&nbsp;&nbsp;Orc&nbsp;&nbsp;&nbsp;&nbsp;Half Orc&nbsp;&nbsp;&nbsp;&nbsp;Gnome)'
+    }
+    else if (a == 'i') {
+        bottom.innerHTML = 's'
+    }
+    else if (a == 'e') {
+        bottom.innerHTML = 's'
+    }
+    else if (a == 'w') {
+        bottom.innerHTML = 's'
+    }
+    else if (a == 'cb') {
+        bottom.innerHTML = 's'
+    }
 }
