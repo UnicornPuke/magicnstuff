a97af058b1a2836869096f9247505c7cac549598
UnicornPuke <41150712+UnicornPuke@users.noreply.github.com>
2023-09-21 02:02:54 +0000

    orc

--- scripts/func.js
+++ scripts/func.js
@@ -4,7 +4,7 @@ function thing(a) {
         bottom.innerHTML = 'Reconstruction&nbsp;&nbsp;&nbsp;&nbsp;Reanimation&nbsp;&nbsp;&nbsp;&nbsp;Destruction&nbsp;&nbsp;&nbsp;&nbsp;Creation&nbsp;&nbsp;&nbsp;&nbsp;Manipulation&nbsp;&nbsp;&nbsp;&nbsp;Naturization&nbsp;&nbsp;&nbsp;&nbsp;Naturization&nbsp;&nbsp;&nbsp;&nbsp;Innovation&nbsp;&nbsp;&nbsp;&nbsp;Transportation'
     }
     else if (a == 'r') {
-        bottom.innerHTML = '<a href="https://redesigned-parakeet-46rgw7jv737gr7-8000.app.github.dev/wiki/races/human">Human</a>&nbsp;&nbsp;&nbsp;&nbsp;Elemental&nbsp;&nbsp;&nbsp;&nbsp;Aplysia&nbsp;&nbsp;&nbsp;&nbsp;Elf&nbsp;&nbsp;&nbsp;&nbsp;Half Elf&nbsp;&nbsp;&nbsp;&nbsp;Gremlin&nbsp;&nbsp;&nbsp;&nbsp;Orc&nbsp;&nbsp;&nbsp;&nbsp;Half Orc&nbsp;&nbsp;&nbsp;&nbsp;Gnome)'
+        bottom.innerHTML = '<a href="https://redesigned-parakeet-46rgw7jv737gr7-8000.app.github.dev/wiki/races/human">Human</a>&nbsp;&nbsp;&nbsp;&nbsp;Elemental&nbsp;&nbsp;&nbsp;&nbsp;Aplysia&nbsp;&nbsp;&nbsp;&nbsp;Elf&nbsp;&nbsp;&nbsp;&nbsp;Half Elf&nbsp;&nbsp;&nbsp;&nbsp;Gremlin&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://redesigned-parakeet-46rgw7jv737gr7-8000.app.github.dev/wiki/races/orc">Orc</a>&nbsp;&nbsp;&nbsp;&nbsp;Half Orc&nbsp;&nbsp;&nbsp;&nbsp;Gnome'
     }
     else if (a == 'i') {
         bottom.innerHTML = 's'
